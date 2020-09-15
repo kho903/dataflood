2f10c7fe49c8117b8df64e0ae73852683056b212
kho903 <gmldnr2222@naver.com>
2020-09-16 01:06:50 +0900

    모든 앱 통합

--- Web/apitest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from apitest.map import indexP
-
-urlpatterns = [
-    path('', indexP, name='apitest'),
-]
--- Web/busanmap/views.py
+++ Web/busanmap/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render
-import pandas as pd
+
+# Create your views here.
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+import fnmatch
 
 
-# # Create your views here.
-# def chartPage(request):
-#     context = {'a': 'a'}
-#     return render(request, 'chart.html')
+class cctv(TemplateView):
+    template_name = 'test/cctv.html'
 
 
-# def indexPage(request):
-#     df = pd.read_excel('PIH_merge.xlsx')
-#     df_i = df[['ZONE', 'IMP_SUR_RATIO']].sort_values(by='ZONE', ascending=True)
-#     df_p = df[['ZONE', 'PUMP_RATIO']].sort_values(by='ZONE', ascending=True)
-#     df_m = df[['ZONE', 'MANHOLES_RATIO']].sort_values(by='ZONE', ascending=True)
-#     zone = df_i['ZONE'].values.tolist()
-#     imp = df_i['IMP_SUR_RATIO'].values.tolist()
-#     pump = df_p['PUMP_RATIO'].values.tolist()
-#     manhole = df_m['MANHOLES_RATIO'].values.tolist()
-#     context = {
-#         'zone': zone,
-#         'imp': imp,
-#         'pump': pump,
-#         'manhole': manhole
-#     }
-#     return render(request, 'busanmap/main.html', context=context)
+def display_video(request, vid=None):
+    if vid is None:
+        return HttpResponse("No Video")
+
+    # Finding the name of video file with extension, use this if you have different extension of the videos
+    video_name = ""
+    for fname in os.listdir(settings.MEDIA_ROOT):
+        if fnmatch.fnmatch(fname, vid + ".mp4"):  # using pattern to find the video file with given id and any extension
+            video_name = fname
+            break
+
+    '''
+        If you have all the videos of same extension e.g. mp4, then instead of above code, you can just use -
+
+        video_name = vid+".mp4"
+
+    '''
+
+    # getting full url -
+    video_url = settings.MEDIA_URL + video_name
+
+    return render(request, "cctv.html", {"url": video_url})
