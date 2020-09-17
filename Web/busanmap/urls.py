06994c1db5291204c43fa6c0ebef983fdccf6b18
kho903 <gmldnr2222@naver.com>
2020-09-18 01:42:42 +0900

    주석 거의 마무리

--- Web/busanmap/urls.py
+++ Web/busanmap/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from busanmap.map import indexPage, indexP, apitest
+from busanmap.map import busan_gu_info, simulation_result, apitest
 from busanmap.views import cctv
 
 urlpatterns = [
-    path('', indexPage, name='BusanMap'),
-    path('model/', indexP, name='modeltest'),
+    # path('url/', map.py에서의 함수이름, name='urlname' : base.html 에서 사용 )
+    path('', busan_gu_info, name='BusanMap'),
+    path('model/', simulation_result, name='modeltest'),
     path('cctv/', cctv.as_view(), name='cctv'),
     path('apitest/', apitest, name='apitest'),
+    # cctv/ 만 views.py 에서 class 호출
 ]
